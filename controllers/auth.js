b34316a4f587fe5c0d1223a050bcd33e82fa48c7
choir241 <choir4558@gmail.com>
2025-12-02 11:41:21 -0500

    add: save refresh, client secret, and merchantId in secure db location. Refresh token endpoint for refreshing oauth token.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const crypto = require("crypto");
-const { oauthClient } = require('../middleware/squareClient');
-const { URL } = require('url');
-const { URLSearchParams } = require('url');
+const { oauthClient, client } = require("../middleware/squareClient");
+const { URL } = require("url");
+const { URLSearchParams } = require("url");
+const { MongoClient } = require("mongodb");
 
 const scopes = [
   "ITEMS_READ",
@@ -20,6 +21,7 @@ module.exports = {
         `https://connect.squareup.com/oauth2/authorize?client_id=${process.env.APP_ID}&` +
         `response_type=code&` +
         `scope=${scopes.join("+")}` +
+        `&session=false` +
         `&state=` +
         state;
 
@@ -30,25 +32,87 @@ module.exports = {
   },
   callback: async (req, res) => {
     try {
+      const connectMongoClient = new MongoClient(process.env.MONGO_URI);
       const url = new URL(req.originalUrl, `http://${req.headers.host}`);
       const params = new URLSearchParams(url.search);
 
-      const code = params.get('code');
+      const code = params.get("code");
 
       if (!code) {
-        return res.status(400).json({ error: 'Auth code missing' });
+        return res.status(400).json({ error: "Auth code missing" });
       }
 
       const token = await oauthClient.oAuth.obtainToken({
         clientId: process.env.APP_ID,
         clientSecret: process.env.APP_SECRET,
         code: code,
-        grantType: "authorization_code"
-      })
+        grantType: "authorization_code",
+      });
 
-      res.json({ token: token });
+      const db = connectMongoClient.db("Supreme-Nomads-Detailing");
+
+      const collection = db.collection("Users");
+
+      const user = await collection.insertOne({
+        accessToken: token.accessToken,
+        refreshToken: token.refreshToken,
+        expiresAt: token.expiresAt,
+        merchantId: token.merchantId,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      if (user) {
+        console.log({
+          message: "User was successfully added to the database.",
+        });
+      }
 
       // {"token":{"accessToken":"","tokenType":"bearer","expiresAt":""","merchantId":"","refreshToken":"","shortLived":false}}
+
+      // Manage and use the access and refresh tokens securely.
+      // Encrypt the access and refresh tokens and store them securely.
+      // database like supabase or appwrite
+
+      req.status(200).json({
+        expiresAt: token.expiresAt,
+        tokenInfo: {
+          tokenType: token.tokenType,
+          shortLived: token.shortLived,
+        },
+      });
+
+      // Verify that the token used for each API call is valid.
+      // Refresh the access token in a timely manner.
+      // Provide the seller with the ability to revoke the access and refresh tokens.
+      // Show the permissions granted by the access token to the seller and enable them to manage authorization.
+      // Ensure that API calls made with the seller's tokens can handle token-based errors appropriately.
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  refreshToken: async (req, res) => {
+    try {
+      const refreshToken = req.body.refreshToken;
+
+      const token = await oauthClient.oAuth.obtainToken({
+        clientId: process.env.APP_ID,
+        clientSecret: process.env.APP_SECRET,
+        refreshToken: refreshToken,
+        grantType: "refresh_token",
+      });
+
+      res.json(token);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  revokeToken: async (req, res) => {
+    try {
+      const token = await oauthClient.oAuth.revokeToken({
+        accessToken: process.env.ACCESS_TOKEN,
+        clientId: process.env.APP_ID,
+      });
     } catch (err) {
       console.error(err);
     }
